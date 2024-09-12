7438aaa8b471d42a6d087db3c933565e260c7c9a
Talha-Abdurehman <talhakyle124@gmail.com>
2024-09-13 02:02:10 +0500

    "Added a HandTracker Module"

--- app.py
+++ app.py
@@ -1,66 +1,28 @@
-import cv2
-import mediapipe as mp
-import threading
-import time
-import mouse
+import time, mediapipe, cv2
+import HandTracker as ht
 
 
-
-
-# Initialize video capture and mediapipe components
+pTime = 0
+cTime = 0
 cap = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-mpDraw = mp.solutions.drawing_utils
-hands = mpHands.Hands()
+detector = ht.HandDetector()
+while True:
+    success, img = cap.read()
 
-# Function to process the frames
-def process_frame():
+    img = detector.findHands(img)
+    position = detector.findPos(img)
 
-    pTime = 0
-    cTime = 0
-    while True:
-        success, img = cap.read()
-        if not success:
-            break
-        
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = hands.process(imgRGB)
+    if len(position) != 0:
+        print(position[2])
 
-        # If hand landmarks are detected
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                for id, lm in enumerate(handLms.landmark):
-                    # print(id, lm)
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    # print(id,cx, cy)
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
 
-                    if id == 8:
-                        print(cx, cy)
-                        mouse.move(cx, cy)
-                        print(f"Current Mouse Loc: {mouse.get_position()}")
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+    # Show the image with landmarks drawn
+    cv2.imshow("Image", img)
 
-        cTime = time.time()
-        fps = 1/(cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
-        # Show the image with landmarks drawn
-        cv2.imshow("Image", img)
-
-        # Break the loop if 'q' key is pressed
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-# Run the process in a separate thread
-thread = threading.Thread(target=process_frame)
-thread.start()
-
-# Join the thread to ensure it closes cleanly
-thread.join()
-
-# Release the video capture and close all OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
-
+    # Break the loop if 'q' key is pressed
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
